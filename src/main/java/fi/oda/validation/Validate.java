c5003729832085f74c6603f66903fe5eab433e85
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-02-24 17:03:05 +0200

    first coommit

---
+++ src/main/java/fi/oda/validation/Validate.java
@@ -0,0 +1,12 @@
+package fi.oda.validation;
+
+import org.hl7.fhir.tools.validator.Validator;
+//import org.hl7.fhir.validator.*; *// no  work
+import org.apache.commons.io.IOUtils;
+
+
+public class Validate{
+	public static void main(String args[]){
+		System.out.println("hello");
+	}
+}
